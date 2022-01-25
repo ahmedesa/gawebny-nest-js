180e8cb7b2f5e64baf0d05b40b0bd16bdabb27c1
ahmed esa <ahmedesa6120@gmail.com>
2022-01-25 13:07:12 +0300

    add docker file

--- src/config/database.module.ts
+++ src/config/database.module.ts
@@ -9,7 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
+        type: 'postgres',
         host: configService.get('DB_HOST'),
         port: configService.get('DB_PORT'),
         username: configService.get('DB_USER'),
